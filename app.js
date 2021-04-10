3c9e04ec020d21854667404e89b24604d9720f0f
Zach Puckett <zpuckettdesign@gmail.com>
2021-04-10 08:28:06 -0500

    feature js

---
+++ app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+
+});
+
+
+/*const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);*/
